4c3452075740cad74f7e94033b7bf9235e4bb792
ramammurshal <rmurshal@gmail.com>
2022-09-05 17:33:13 +0700

    init tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 class="text-3xl">Pokedex App!</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
